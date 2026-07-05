12f868cabe26321d2e8d9ebcc2c71e3c3bc86a71
Zexolver <Zexolver@Disroot.org>
2026-07-05 12:25:59 -0400

    fix: revert blanket nixpkgs follows injection to prevent legacy build failures

--- rix-core/src/ops/flake.rs
+++ rix-core/src/ops/flake.rs
@@ -62,11 +62,7 @@ pub fn add_external_input(
     let mut content = fs::read_to_string(&flake_path)?;
 
     // 1. Inject the Input Attribute
-    // 1. Inject the Input Attribute
-    let input_str = format!(
-        "   {}.url = \"{}\";\n    {}.inputs.nixpkgs.follows = \"nixpkgs\";\n  ",
-        alias, uri, alias
-    );
+    let input_str = format!("   {}.url = \"{}\";\n  ", alias, uri);
     if !content.contains(&format!("{}.url", alias)) {
         if let Some(inputs_end_idx) = content.find("  };\n\n  outputs = {") {
             content.insert_str(inputs_end_idx, &input_str);
